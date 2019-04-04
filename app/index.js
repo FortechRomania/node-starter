7635abef945982ef24c89cab3299b1d03470c63e
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-04-04 17:23:12 +0300

    solved issue #11

--- app/index.js
+++ app/index.js
@@ -1,41 +1,42 @@
-const express = require( "express" );
-const bodyParser = require( "body-parser" );
-const logger = require( "./utilities/logger" );
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('./utilities/logger');
 
-const config = require( "./config" );
-const customResponses = require( "./middlewares/customResponses" );
+const config = require('./config');
+const customResponses = require('./middlewares/customResponses');
 
-const app = express( );
+const app = express();
 const port = process.env.PORT || config.port;
-const ENV = process.env.NODE_ENV || config.env;
+const ENV = config.env;
 
-app.set( "env", ENV );
+app.set('env', ENV);
 
-require( "./models/user" );
+require('./models/user');
 // add all models that are used in the app. Use require as below:
 // require( path to model )
 
-app.use( bodyParser.json( ) );
-app.use( customResponses );
+app.use(bodyParser.json());
+app.use(customResponses);
 
-require( "./config/mongoose" )( app );
-require( "./config/routes" )( app );
+require('./config/mongoose')(app);
+require('./config/routes')(app);
 
-app.use( ( req, res ) => {
-    res.notFound( );
-} );
+app.use((req, res) => {
+  res.notFound();
+});
 
-app.use( ( err, req, res, next ) => {
-    logger.error( err.stack );
-    next( err );
-} );
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  next(err);
+});
 
 // Don't remove next !!!!
-app.use( ( err, req, res, next ) => { // eslint-disable-line no-unused-vars
-    res.status( 503 ).json( {
-        success: false,
-        error: "server_error",
-    } );
-} );
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  res.status(503).json({
+    success: false,
+    error: 'server_error'
+  });
+});
 
-app.listen( port );
+app.listen(port);
