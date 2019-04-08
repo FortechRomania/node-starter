ec90dd77bd5551010fbb7c83ff30edd7ce6ced98
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-04-08 12:47:48 +0300

    fixed error

--- app/index.js
+++ app/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('./utilities/logger');
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("./utilities/logger");
 
-const config = require('./config');
-const customResponses = require('./middlewares/customResponses');
+const config = require("./config");
+const customResponses = require("./middlewares/customResponses");
 
 const app = express();
 const port = process.env.PORT || config.port;
 const ENV = config.env;
 
-app.set('env', ENV);
+app.set("env", ENV);
 
-require('./models/user');
+require("./models/user");
 // add all models that are used in the app. Use require as below:
 // require( path to model )
 
 app.use(bodyParser.json());
 app.use(customResponses);
 
-require('./config/mongoose')(app);
-require('./config/routes')(app);
+require("./config/mongoose")(app);
+require("./config/routes")(app);
 
 app.use((req, res) => {
   res.notFound();
@@ -30,12 +30,12 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// Don't remove next !!!!
+// Don"t remove next !!!!
 app.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   res.status(503).json({
     success: false,
-    error: 'server_error'
+    error: "server_error"
   });
 });
 
